c713b0497a0daffa761782772d330713e78381c1
Ted Sazon <tedsazon@gmail.com>
2018-12-17 20:12:15 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+def my_collect(array)
+  if block_given?
+    x = 0
+  new_collection = []
 
+    while x < array.length
+      new_collection  << yield(array[x])
+      x += 1
+    end
+  end
+  new_collection
 
+end
