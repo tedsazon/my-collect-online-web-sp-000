483ed340b3ecc6168840a656137287fe2b52571d
Ted Sazon <tedsazon@gmail.com>
2018-12-17 20:10:48 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,13 @@
 def my_collect(array)
   if block_given?
-  x = 0
+    x = 0
   new_collection = []
 
+    while x < array.length
+    new_collection  << yield(array[x])
+      x += 1
+    end
+    
 
 
 end
